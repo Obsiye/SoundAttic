16f844a299a95cf7a3ba17ba03c161e5f1cacc46
Obsiye <mrabdirizak@gmail.com>
2018-11-20 10:50:52 +0000

    Pass sounds_spec tests and add feature test for sounds

--- spec/features/sounds_spec.rb
+++ spec/features/sounds_spec.rb
@@ -2,9 +2,15 @@ feature 'Sounds' do
   
   scenario 'display a list of sound titles' do
     visit('/sounds')
-    expect(page).to have_content 'biglife.mp3'
-    expect(page).to have_content 'havocroad.mp3'
-    expect(page).to have_content 'lilnoway.mp3'
+    expect(page).to have_content('biglife.mp3')
+    expect(page).to have_content('havocroad.mp3')
+    expect(page).to have_content('lilnoway.mp3')
+  end
+
+  scenario 'return back to index page' do
+    visit('/sounds')
+    expect(page).to have_content('SoundAttic')
+    expect(page).not_to have_content('biglife.mp3')
   end
 
 end
--- spec/sounds_spec.rb
+++ spec/sounds_spec.rb
@@ -4,10 +4,8 @@ describe Sounds do
   describe '#all' do
     it 'returns an array of bookmarks' do
       sounds = Sounds.all
-      
-      expect(sounds).to include("biglife.mp3")
-      expect(sounds).to include("havocroad.mp3")
-      expect(sounds).to include("lilnoway.mp3")
+
+      expect(sounds.any? { |hash| hash['title'] == 'biglife.mp3' }).to be true
     end
   end
 end
